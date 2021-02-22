1eaae55c922233f482633d46434217d3edb11c14
Immo Landwerth <immol@microsoft.com>
2021-02-22 09:41:26 -0800

    Initial commit

---
+++ WinFormsApp1/SomeInteropWithExcel.cs
@@ -0,0 +1,13 @@
+﻿using Excel = Microsoft.Office.Interop.Excel;
+
+class SomeInteropWithExcel
+{
+    public void Test()
+    {
+        Excel.Application xlApp = new Excel.Application { Visible = true };
+        Excel.Workbook xlBook = xlApp.Workbooks[1];
+        Excel.Worksheet sheet = xlBook.Sheets[1] as Excel.Worksheet;
+        int lastRow = sheet.Cells[sheet.Rows.Count, "Q"].End[Excel.XlDirection.xlUp].Row;
+        dynamic value = sheet.Cells[lastRow, "Q"].Value;
+    }
+}
